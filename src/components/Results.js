87cba01d88df5eb885f005843d1b67b037013f09
Matthew <mattsjsu14@gmail.com>
2019-08-27 20:35:50 -0700

    retrieved gifs from api

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Results = () => {
-  return (
-    <div className='results-container'>
+
+  const [trendingGifs, setTrendingGifs] = useState([])
+  console.log(trendingGifs)
+
+
+  useEffect(() => {
+    const fetchTrendingGifs = async () => {
+      const res = await fetch('https://api.giphy.com/v1/gifs/trending?api_key=qKA0xNCx1jDl9sK7kz6UObcKKTCBRk3y&limit=30&rating=G')
+      const data = await res.json();
+      console.log(data)
       
+      setTrendingGifs(data.data)
+    }
+
+    fetchTrendingGifs()
+  }, [])
+
+  return (
+    <div className='results-container' >
+      {trendingGifs.map(gif =>{
+        return(
+        <div className='gif' key={gif.id}>
+          <img alt='gif' src={gif.images.fixed_height.url} />
+        </div>
+        )
+      })}
     </div>
   )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import '../App.css';
 
 
 const SearchBar = () => {
+
+  const [searchGif, setSearchGif] = useState('')
+  const [isSearchClicked, setIsSearchClicked] = useState(false)
+  const searchInput = useRef(null)
+
+  const handleSearchButton = (e) => {
+    e.preventDefault()
+    setSearchGif(searchInput.current.value)
+    setIsSearchClicked(true)
+    searchInput.current.value = ''
+  }
+
   return (
     <div>
-      <form className='search-form'>
-        <input className='search-bar' type='search' placeholder='Search for a gif' />
-        <button className='search-button'><i className="fas fa-search"></i></button>
+      <form className='search-form' onSubmit={handleSearchButton}>
+        <input ref={searchInput} className='search-bar' type='search' placeholder='Search for a gif' />
+        <button type='submit' className='search-button'><i className="fas fa-search"></i></button>
       </form>
     </div>
   )
